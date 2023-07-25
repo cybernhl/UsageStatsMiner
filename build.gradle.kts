d81bcb2c12b0bd3617b2274c0a2a28fa874a759a
Neo <c.min.tseng@gmail.com>
2023-07-25 10:50:03 +0800

    modify import plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
+val kotlin_version: String by project
+val ktor_version: String by project
+
+val logback_version: String by project
+
+val sqlDelight_version: String by project
+val kmongo_version: String by project
+
 plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.0.2").apply(false)
     id("com.android.library").version("8.0.2").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
-}
-
-buildscript {
-    dependencies {
-        classpath("com.squareup.sqldelight:gradle-plugin:1.5.5")
-    }
+    id("org.jetbrains.kotlin.plugin.serialization").version("1.8.21").apply(false)
+    id("com.squareup.sqldelight").version("1.5.5").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
-    kotlin("plugin.serialization") version "1.8.21"
+    kotlin("plugin.serialization")
     id("com.squareup.sqldelight")
 }
 
