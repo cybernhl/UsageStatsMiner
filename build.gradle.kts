08030ab56071ab76bbef69e97281733dbde0a080
Neo <c.min.tseng@gmail.com>
2023-07-25 12:00:36 +0800

    add support jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,12 @@ plugins {
     //trick: for the same plugin versions in all sub-modules
     id("com.android.application").version("8.0.2").apply(false)
     id("com.android.library").version("8.0.2").apply(false)
+    kotlin("jvm").version("1.8.21").apply(false)
     kotlin("android").version("1.8.21").apply(false)
     kotlin("multiplatform").version("1.8.21").apply(false)
     id("org.jetbrains.kotlin.plugin.serialization").version("1.8.21").apply(false)
     id("com.squareup.sqldelight").version("1.5.5").apply(false)
+    id("io.ktor.plugin").version("2.3.2").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
