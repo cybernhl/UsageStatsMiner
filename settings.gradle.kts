51eebcdc56d22220034ca015542262b070f8157d
Neo <c.min.tseng@gmail.com>
2023-08-01 02:10:27 +0800

    add check JDK version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,14 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+val fullVersion = System.getProperty("java.version", "8.0.0")
+val versionComponents = fullVersion
+    .split(".")
+    .take(2)
+    .filter { it.isNotBlank() }
+    .map { Integer.parseInt(it) }
 
+val currentJdk = if (versionComponents[0] == 1) versionComponents[1] else versionComponents[0]
 rootProject.name = "UsageStatsMiner"
 //share
 include(":shared")
